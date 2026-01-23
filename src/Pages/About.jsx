926001605b5e83076714bfddcd0ecbf4ad122a3d
yasser <yasserala3mer@gmail.com>
2026-01-23 11:47:15 +0300

    Add main Pages

---
+++ src/Pages/About.jsx
@@ -0,0 +1,244 @@
+import React from 'react'
+// REACT FONYAWSOME IMPORTS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebook,
+  faInstagram,
+  faXTwitter
+} from '@fortawesome/free-brands-svg-icons'
+import logo3 from "../logo3.png"
+import logo4 from "../logo4.png"
+import logo from "../logo.png"
+
+const About = () => {
+  return (
+    <>
+            {/* about */}
+        <div className="container-about">
+          <div className="about">
+            <div className="about-img">
+              <img src={logo3} alt="" className='logo' />
+            </div>
+            <div className="about-text">
+              <h2 className="text-title">
+                about us
+              </h2>
+              <p className='text'>
+                At our real estate company in Damascus, Syria,
+                 we pride ourselves on being a trusted destination for those looking to buy,
+                  sell, or rent homes and properties.
+                   We are dedicated to providing a professional and reliable real estate experience in a comfortable and welcoming environment,
+                    where clients can feel confident,
+                 take their time,
+                  and find the property that best suits their needs with ease and peace of mind
+              </p>
+              <div className="social-link-list">
+                <a href="#" className="social-link">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a href="#" className="social-link">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="#" className="social-link">
+                  <FontAwesomeIcon icon={faXTwitter} />
+                </a>
+              </div>         
+            </div>
+          </div>
+        </div>
+
+     {/* Model */}
+    <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title" id="staticBackdropLabel">Dream Apartment</h1>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+             <div className="modal-body">
+              {/* Carousel */}
+              <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>                                                                        
+                </div>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img src={logo} className="d-block w-100" />
+                  </div>
+                  <div className="carousel-item">
+                    <img src={logo3} className="d-block w-100" />
+                  </div>
+                  <div className="carousel-item">
+                    <img src={logo4} className="d-block w-100" />
+                  </div>
+                  <div className="carousel-item">
+                    <img src={logo4} className="d-block w-100" />
+                  </div>
+                  <div className="carousel-item">
+                    <img src={logo4} className="d-block w-100" />
+                  </div>
+                  <div className="carousel-item">
+                    <img src={logo4} className="d-block w-100" />
+                  </div>
+                  <div className="carousel-item">
+                    <img src={logo4} className="d-block w-100" />
+                  </div>                                                                        
+                </div>
+              </div>
+              {/* Collapse */}
+              <div className="collapse-container" id="accordionExample">
+                <a className="btn" data-bs-toggle="collapse" href="#Specifications" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Specifications
+                </a>
+                <a className="btn" data-bs-toggle="collapse" href="#Houselocation" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  House location
+                </a>
+                <a className="btn" data-bs-toggle="collapse" href="#Houseadescriotion" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  House a descriotion
+                </a>
+                <a className="btn" data-bs-toggle="collapse" href="#contactinformation" role="button" aria-expanded="false" aria-controls="collapseExample">
+                  Contact information
+                </a>  
+                <div className="collapse" id="Specifications"  data-bs-parent="#accordionExample">
+                  <div className="card card-body">
+                    <div className="property-type">
+                      <h6 className="title-proerty">
+                        Property type
+                      </h6>
+                      <p>
+                        apartment
+                      </p>
+                    </div>
+                    <div className="price">
+                      <h6 className="price-title">
+                        Price
+                      </h6>
+                      <p>25000$</p>
+                    </div>
+                    <div className="space">
+                      <h6 className="space-title">
+                        Space
+                      </h6>
+                      <p>200 M</p>
+                    </div>
+                    <div className="rooms">
+                      <h6 className="room-title">
+                        Rooms
+                      </h6>
+                      <p>
+                        5
+                      </p>
+                    </div>
+                    <div className="baths">
+                      <h6 className="bath-title">
+                        Bath
+                      </h6>
+                      <p>
+                        2
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="collapse" id='Houselocation'  data-bs-parent="#accordionExample" >
+                  <div className="card card-body ">
+                    <div className="city">
+                      <h6 className="city-title">
+                        City
+                      </h6>
+                      <p>Damacous</p>
+                    </div>
+                    <div className="area">
+                      <h6 className="area-title">
+                        Area
+                      </h6>
+                      <p>
+                        mazze
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="collapse" id='Houseadescriotion'  data-bs-parent="#accordionExample">
+                  <div className="card card-body">
+                    <div className="House-a-descriotion">
+                      <p>
+                        Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                      </p>
+                    </div>
+                  </div>
+                </div>                 
+                <div className="collapse" id='contactinformation'  data-bs-parent="#accordionExample">
+                  <div className="card card-body">
+                    <div className="number-phone">
+                      <h6 className="numberphone-title">
+                        Number Phone
+                      </h6>
+                      <p>
+                        +963 993 8896 61
+                      </p>
+                    </div>
+                  </div>
+                </div>               
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>        
+     {/* Offcanvas */}
+<div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div className="offcanvas-header">
+    <h6 className="offcanvas-title" id="offcanvasExampleLabel">Your basket</h6>
+    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div className="offcanvas-body">
+    <div className="card" style={{width:"18rem" }}>
+      <img src={logo4} className='logo'  alt="logo4"/>
+      <div className="card-body">
+        <h6 className="card-title">
+          Dream Apartment
+          </h6>
+          <p className="card-text">
+            Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+            </p>
+            <a href="#" className='btn btn-offcanvas' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+      </div>
+    </div>
+     
+    <div className="card" style={{width:"18rem" }}>
+      <img src={logo4} className='logo'  alt="logo4"/>
+      <div className="card-body">
+        <h6 className="card-title">
+          Dream Apartment
+          </h6>
+          <p className="card-text">
+            Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+            </p>
+            <a href="#" className='btn btn-offcanvas' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+      </div>
+    </div> 
+
+    <div className="card" style={{width:"18rem" }}>
+      <img src={logo4} className='logo'  alt="logo4"/>
+      <div className="card-body">
+        <h6 className="card-title">
+          Dream Apartment
+          </h6>
+          <p className="card-text">
+            Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+            </p>
+            <a href="#" className='btn btn-offcanvas' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+      </div>
+    </div>     
+
+  </div>
+</div>
+     
+    </>
+  )
+}
+export default About;
---
+++ src/Pages/Residences.jsx
@@ -0,0 +1,310 @@
+import React from 'react'
+// REACT FONYAWSOME IMPORTS
+
+import logo from "../logo.png"
+import logo3 from "../logo3.png"
+import logo4 from "../logo4.png"
+const Residences = () => {
+  return (
+    <>
+        {/* Residences */}
+        <header>
+          <h2 className="text-title">
+            Residences        
+          </h2>
+        </header>
+        <div className="container container-Residences">
+          <div className="Residences">
+            <div className="card" style={{width:"18rem" }}>
+              <img src={logo4} className='logo'  alt="logo4"/>
+              <div className="card-body">
+                <h5 className="card-title">
+                  Dream Apartment
+                </h5>
+                <p className="card-text">
+                  Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                </p>
+                <a href="#" className='btn' data-bs-toggle="modal" data-bs-target="#staticBackdrop" >Explore the Apartment</a>
+              </div>
+            </div>
+            <div className="card" style={{width:"18rem" }}>
+              <img src={logo4} className='logo'  alt="logo4"/>
+              <div className="card-body">
+                <h5 className="card-title">
+                  Dream Apartment
+                </h5>
+                <p className="card-text">
+                  Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                </p>
+                <a href="#" className='btn'data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+              </div>
+            </div>   
+            <div className="card" style={{width:"18rem" }}>
+              <img src={logo4} className='logo'  alt="logo4"/>
+              <div className="card-body">
+                <h5 className="card-title">
+                  Dream Apartment
+                </h5>
+                <p className="card-text">
+                  Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                </p>
+                <a href="#" className='btn' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+              </div>
+            </div>   
+            <div className="card" style={{width:"18rem" }}>
+              <img src={logo4} className='logo'  alt="logo4"/>
+              <div className="card-body">
+                <h5 className="card-title">
+                  Dream Apartment
+                </h5>
+                <p className="card-text">
+                  Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                </p>
+                <a href="#" className='btn' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+              </div>
+            </div>   
+            <div className="card" style={{width:"18rem" }}>
+              <img src={logo4} className='logo'  alt="logo4"/>
+              <div className="card-body">
+                <h5 className="card-title">
+                  Dream Apartment
+                </h5>
+                <p className="card-text">
+                  Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                </p>
+                <a href="#" className='btn' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+              </div>
+            </div>   
+            <div className="card" style={{width:"18rem" }}>
+              <img src={logo4} className='logo'  alt="logo4"/>
+              <div className="card-body">
+                <h5 className="card-title">
+                  Dream Apartment
+                </h5>
+                <p className="card-text">
+                  Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                </p>
+                <a href="#" className='btn' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+              </div>
+            </div>   
+            <div className="card" style={{width:"18rem" }}>
+              <img src={logo4} className='logo'  alt="logo4"/>
+              <div className="card-body">
+                <h5 className="card-title">
+                  Dream Apartment
+                </h5>
+                <p className="card-text">
+                  Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                </p>
+                <a href="#" className='btn' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+              </div>
+            </div>   
+            <div className="card" style={{width:"18rem" }}>
+              <img src={logo4} className='logo'  alt="logo4"/>
+              <div className="card-body">
+                <h5 className="card-title">
+                  Dream Apartment
+                </h5>
+                <p className="card-text">
+                  Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                </p>
+                <a href="#" className='btn' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+              </div>
+            </div>                                                                
+          </div>      
+        </div>
+    
+      {/* Model */}
+     <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+       <div className="modal-dialog">
+         <div className="modal-content">
+           <div className="modal-header">
+             <h1 className="modal-title" id="staticBackdropLabel">Dream Apartment</h1>
+             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+           </div>
+              <div className="modal-body">
+               {/* Carousel */}
+               <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+                 <div className="carousel-indicators">
+                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>                                                                        
+                 </div>
+                 <div className="carousel-inner">
+                   <div className="carousel-item active">
+                     <img src={logo} className="d-block w-100" />
+                   </div>
+                   <div className="carousel-item">
+                     <img src={logo3} className="d-block w-100" />
+                   </div>
+                   <div className="carousel-item">
+                     <img src={logo4} className="d-block w-100" />
+                   </div>
+                   <div className="carousel-item">
+                     <img src={logo4} className="d-block w-100" />
+                   </div>
+                   <div className="carousel-item">
+                     <img src={logo4} className="d-block w-100" />
+                   </div>
+                   <div className="carousel-item">
+                     <img src={logo4} className="d-block w-100" />
+                   </div>
+                   <div className="carousel-item">
+                     <img src={logo4} className="d-block w-100" />
+                   </div>                                                                        
+                 </div>
+               </div>
+               {/* Collapse */}
+               <div className="collapse-container" id="accordionExample">
+                 <a className="btn" data-bs-toggle="collapse" href="#Specifications" role="button" aria-expanded="false" aria-controls="collapseExample">
+                   Specifications
+                 </a>
+                 <a className="btn" data-bs-toggle="collapse" href="#Houselocation" role="button" aria-expanded="false" aria-controls="collapseExample">
+                   House location
+                 </a>
+                 <a className="btn" data-bs-toggle="collapse" href="#Houseadescriotion" role="button" aria-expanded="false" aria-controls="collapseExample">
+                   House a descriotion
+                 </a>
+                 <a className="btn" data-bs-toggle="collapse" href="#contactinformation" role="button" aria-expanded="false" aria-controls="collapseExample">
+                   Contact information
+                 </a>  
+                 <div className="collapse" id="Specifications"  data-bs-parent="#accordionExample">
+                   <div className="card card-body">
+                     <div className="property-type">
+                       <h6 className="title-proerty">
+                         Property type
+                       </h6>
+                       <p>
+                         apartment
+                       </p>
+                     </div>
+                     <div className="price">
+                       <h6 className="price-title">
+                         Price
+                       </h6>
+                       <p>25000$</p>
+                     </div>
+                     <div className="space">
+                       <h6 className="space-title">
+                         Space
+                       </h6>
+                       <p>200 M</p>
+                     </div>
+                     <div className="rooms">
+                       <h6 className="room-title">
+                         Rooms
+                       </h6>
+                       <p>
+                         5
+                       </p>
+                     </div>
+                     <div className="baths">
+                       <h6 className="bath-title">
+                         Bath
+                       </h6>
+                       <p>
+                         2
+                       </p>
+                     </div>
+                   </div>
+                 </div>
+                 <div className="collapse" id='Houselocation'  data-bs-parent="#accordionExample" >
+                   <div className="card card-body ">
+                     <div className="city">
+                       <h6 className="city-title">
+                         City
+                       </h6>
+                       <p>Damacous</p>
+                     </div>
+                     <div className="area">
+                       <h6 className="area-title">
+                         Area
+                       </h6>
+                       <p>
+                         mazze
+                       </p>
+                     </div>
+                   </div>
+                 </div>
+                 <div className="collapse" id='Houseadescriotion'  data-bs-parent="#accordionExample">
+                   <div className="card card-body">
+                     <div className="House-a-descriotion">
+                       <p>
+                         Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+                       </p>
+                     </div>
+                   </div>
+                 </div>                 
+                 <div className="collapse" id='contactinformation'  data-bs-parent="#accordionExample">
+                   <div className="card card-body">
+                     <div className="number-phone">
+                       <h6 className="numberphone-title">
+                         Number Phone
+                       </h6>
+                       <p>
+                         +963 993 8896 61
+                       </p>
+                     </div>
+                   </div>
+                 </div>               
+               </div>
+           </div>
+         </div>
+       </div>
+     </div>        
+      {/* Offcanvas */}
+ <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+   <div className="offcanvas-header">
+     <h6 className="offcanvas-title" id="offcanvasExampleLabel">Your basket</h6>
+     <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+   </div>
+   <div className="offcanvas-body">
+     <div className="card" style={{width:"18rem" }}>
+       <img src={logo4} className='logo'  alt="logo4"/>
+       <div className="card-body">
+         <h6 className="card-title">
+           Dream Apartment
+           </h6>
+           <p className="card-text">
+             Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+             </p>
+             <a href="#" className='btn btn-offcanvas' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+       </div>
+     </div>
+      
+     <div className="card" style={{width:"18rem" }}>
+       <img src={logo4} className='logo'  alt="logo4"/>
+       <div className="card-body">
+         <h6 className="card-title">
+           Dream Apartment
+           </h6>
+           <p className="card-text">
+             Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+             </p>
+             <a href="#" className='btn btn-offcanvas' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+       </div>
+     </div> 
+ 
+     <div className="card" style={{width:"18rem" }}>
+       <img src={logo4} className='logo'  alt="logo4"/>
+       <div className="card-body">
+         <h6 className="card-title">
+           Dream Apartment
+           </h6>
+           <p className="card-text">
+             Modern, Peaceful Apartment with Natural Light and Spacious Comfort
+             </p>
+             <a href="#" className='btn btn-offcanvas' data-bs-toggle="modal" data-bs-target="#staticBackdrop"  >Explore the Apartment</a>
+       </div>
+     </div>     
+ 
+   </div>
+ </div>       
+    </>
+  )
+}
+export default Residences;
